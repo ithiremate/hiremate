3414c716ce35a5071163427ddcdfa3ef06447b7f
makspooh <maksimpooh@gmail.com>
2023-12-12 21:57:30 +0200

    Select shared component added, Select and Button to Autoscreening modal added

--- src/components/containers/ModalContainer/modals/JobAutoscreeningModal/index.js
+++ src/components/containers/ModalContainer/modals/JobAutoscreeningModal/index.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-unused-vars */
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
+import POST_JOB from "../../../../../utils/constants/postJob";
 import { hideModal } from "../../../../../store/slices/modalSlice";
 
+import Select from "../../../../shared/Select";
 import Button from "../../../../shared/Button";
 
 import styles from "./index.module.scss";
@@ -11,14 +15,34 @@ function JobAutoscreeningModal() {
 
   const { data } = useSelector((state) => state.modal);
 
+  const [internalQuestions, setInternalQuestions] = useState(data?.questions);
+  const [questionType, setQuestionType] = useState({ value: "", label: "" });
+
   const handleSubmit = () => {
     data.onSubmit({ questions: [{ value: "SUBMITTED!" }] });
     dispatch(hideModal());
   };
 
+  const handleQuestionTypeChange = ({ value }) => {
+    setQuestionType(value);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        <div className={styles.newQuestionContainer}>
+          <Select
+            label="Chose question type"
+            placeholder="Question type"
+            options={POST_JOB.AUTOSCREENING_QUESTIONS_OPTIONS}
+            value={questionType}
+            onChange={handleQuestionTypeChange}
+            isRequired
+          />
+
+          <Button label="add question" />
+        </div>
+      </div>
 
       <div className={styles.buttons}>
         <Button className={styles.button} label="save" onClick={handleSubmit} />
---
+++ src/components/shared/Select/index.js
@@ -0,0 +1,124 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import { useState, useRef } from "react";
+import classNames from "classnames";
+
+import useClickOutside from "../../../hooks/useClickOutside";
+
+import SvgIcon from "../SvgIcon";
+
+import styles from "./index.module.scss";
+
+function Select({
+  label,
+  placeholder,
+  options,
+  value,
+  valueKey,
+  isRequired,
+  onChange,
+}) {
+  const { currentTheme } = useSelector((state) => state.theme);
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const clickOutsideRef = useRef(null);
+
+  const toggleOptions = () => setIsExpanded((prev) => !prev);
+
+  const handleChange = (option) => () => {
+    setIsExpanded(false);
+    onChange({ value: option, valueKey });
+  };
+
+  useClickOutside(clickOutsideRef, () => setIsExpanded(false));
+
+  return (
+    <div ref={clickOutsideRef} className={styles.container}>
+      <span
+        className={classNames(styles.label, styles[`label_${currentTheme}`])}
+        onClick={() => setIsExpanded(true)}>
+        {label}
+        {isRequired && (
+          <span className={styles[`asterisk_${currentTheme}`]}>*</span>
+        )}
+      </span>
+
+      <div className={styles.fieldContainer}>
+        <div
+          className={classNames(styles.field, styles[`field_${currentTheme}`])}
+          onClick={toggleOptions}>
+          <span
+            className={classNames(styles.value, {
+              [styles.placeholder]: !value.value,
+              [styles[`placeholder_${currentTheme}`]]: !value.value,
+            })}>
+            {value.label || placeholder}
+          </span>
+
+          <SvgIcon
+            type="chevron"
+            className={classNames(styles.arrow, {
+              [styles.arrow_expanded]: isExpanded,
+            })}
+          />
+        </div>
+
+        <div
+          className={classNames(
+            styles.options,
+            styles[`options_${currentTheme}`],
+            {
+              [styles.options_expanded]: isExpanded,
+            },
+          )}>
+          {options.map((option) => {
+            return (
+              <div
+                key={option.value}
+                className={classNames(
+                  styles.option,
+                  styles[`option_${currentTheme}`],
+                )}
+                onClick={handleChange(option)}>
+                <span>{option.label}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Select.propTypes = {
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    }),
+  ),
+  value: PropTypes.shape({
+    value: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+  }),
+  valueKey: PropTypes.string,
+  isRequired: PropTypes.bool,
+  onChange: PropTypes.func,
+};
+
+Select.defaultProps = {
+  label: "",
+  placeholder: "",
+  options: [{ value: "", label: "" }],
+  value: { value: "", label: "" },
+  valueKey: "",
+  isRequired: false,
+  onChange: () => {},
+};
+
+export default Select;
